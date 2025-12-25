3e92da8ae344b1458290f0aefc182cc0fa93984c
lulunac27a <100660343+lulunac27a@users.noreply.github.com>
2025-12-25 17:22:26 -0600

    Add output text

--- index.js
+++ index.js
@@ -2,8 +2,10 @@ let cards = [];
 const nameElement = document.getElementById("name");
 const rarityOption = document.getElementById("rarity");
 const addCardButton = document.getElementById("add-card");
+const output = document.getElementById("output");
 addCardButton.addEventListener("click", () => {
     const name = nameElement.value;
     const rarity = rarityOption.value;
     cards.push({ name: name, rarity: rarity });
+    output.innerText = JSON.stringify(cards);
 });
