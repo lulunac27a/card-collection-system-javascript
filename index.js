672ef0f0e4931af27fd41249f6c37f927f6ba25c
lulunac27a <100660343+lulunac27a@users.noreply.github.com>
2025-12-29 20:16:05 -0600

    Add button to generate random card based on rarity

--- index.js
+++ index.js
@@ -2,7 +2,11 @@ let cards = []; //list of cards in collection
 const nameElement = document.getElementById("name"); //name element
 const rarityOption = document.getElementById("rarity"); //rarity option selection
 const addCardButton = document.getElementById("add-card"); //add card button
+const generateRandomCardButton = document.getElementById(
+    "generate-random-card",
+); //generate random card button
 const output = document.getElementById("output"); //output display text
+const randomCardOutput = document.getElementById("random-card-output"); //random card output display text
 addCardButton.addEventListener("click", () => {
     //when add card button is pressed
     const name = nameElement.value; //get name value
@@ -11,4 +15,44 @@ addCardButton.addEventListener("click", () => {
     output.innerText = `Number of cards: ${cards.length}\n${JSON.stringify(cards, null, 2)}`; //update output text
     nameElement.value = ""; //clear name value
     rarityOption.value = "common"; //reset rarity value
+});
+generateRandomCardButton.addEventListener("click", () => {
+    //when generate random card button is pressed
+    const totalWeight = cards.reduce(
+        (total, card) =>
+            total + card.rarity === "common"
+                ? 0.5
+                : card.rarity === "uncommon"
+                  ? 0.25
+                  : card.rarity === "rare"
+                    ? 0.15
+                    : card.rarity === "epic"
+                      ? 0.075
+                      : card.rarity === "legendary"
+                        ? 0.025
+                        : 0,
+        0,
+    ); //calculate total weight of all cards based on card rarity
+    const number = Math.random() * totalWeight; //generate random number between 0 and total weight
+    let weightSum = 0; //initialize weight sum
+    for (const card of cards) {
+        //for each card in collection
+        weightSum +=
+            card.rarity === "common"
+                ? 0.5
+                : card.rarity === "uncommon"
+                  ? 0.25
+                  : card.rarity === "rare"
+                    ? 0.15
+                    : card.rarity === "epic"
+                      ? 0.075
+                      : card.rarity === "legendary"
+                        ? 0.025
+                        : 0; //add card weight to weight sum
+        if (number <= weightSum) {
+            //if random number is less than or equal to weight sum
+            randomCardOutput.innerText = `You got a ${card.rarity} card: ${card.name}`; //display generated random card
+            return; //exit function
+        }
+    }
 });
